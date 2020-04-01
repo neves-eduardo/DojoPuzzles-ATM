3939a4d170f3822d62380f44438673b702cd27e4
neves-eduardo <eduardoneves858@gmail.com>
2020-04-01 02:02:33 -0300

    feat: adding user interaction

--- src/main/java/com/neves_eduardo/cash_machine/Main.java
+++ src/main/java/com/neves_eduardo/cash_machine/Main.java
@@ -1,7 +1,24 @@
 package com.neves_eduardo.cash_machine;
 
+import com.neves_eduardo.cash_machine.exception.NoNotesForTransactionException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        CashMachine cashMachine = new CashMachine();
+        System.out.println("Welcome to The Bank \nYour balance: infinite");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please insert the desired amount to withdraw:");
+            int z = scanner.nextInt();
+            System.out.println(cashMachine.withdraw(z));
+        } catch (InputMismatchException ex) {
+            System.out.println("Invalid input, please insert only numbers");
+        } catch (NoNotesForTransactionException ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
