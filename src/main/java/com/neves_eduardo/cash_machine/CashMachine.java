50709f409876cf500fde9cc35b4d9caf869030a5
neves-eduardo <eduardoneves858@gmail.com>
2020-04-01 18:10:46 -0300

    feat: adding validation to when the machine is empty

--- src/main/java/com/neves_eduardo/cash_machine/CashMachine.java
+++ src/main/java/com/neves_eduardo/cash_machine/CashMachine.java
@@ -1,6 +1,7 @@
 package com.neves_eduardo.cash_machine;
 
 import com.google.common.collect.Iterables;
+import com.neves_eduardo.cash_machine.exception.MachineEmptyException;
 import com.neves_eduardo.cash_machine.exception.NoNotesForTransactionException;
 import org.apache.commons.lang3.EnumUtils;
 
@@ -20,6 +21,8 @@ public class CashMachine {
     }
 
     public Map<String,Integer> withdraw(int quantity) {
+        if(totalAmountOfCash == 0) {throw new MachineEmptyException("Machine Empty! Please use another one.");}
+
         Map<String, Integer> notesQuantity = new HashMap<>();
         int smallestAvailableNoteValue = listOfNotes.stream().filter(s -> s.getNumberOfNotesStored() != 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList()).get(0).getValue();
         int remaining = quantity;
@@ -39,7 +42,7 @@ public class CashMachine {
         for ( AvailableNotes note: listOfNotes) {
             int numberOfNotes = countNotes(remaining,note);
             remaining -= numberOfNotes*note.getValue();
-            notesQuantity.put(note.name(),numberOfNotes);
+            if(numberOfNotes != 0) notesQuantity.put(note.name(),numberOfNotes);
             if(remaining == 0) break;
         }
         totalAmountOfCash -= quantity;
