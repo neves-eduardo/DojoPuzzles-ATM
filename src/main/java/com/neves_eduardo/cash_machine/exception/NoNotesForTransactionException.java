88df3dfba272e29f2484f6eedb0185539d0caa45
neves-eduardo <eduardoneves858@gmail.com>
2020-04-01 02:01:46 -0300

    feat: adding exceptions for when there are no notes to cover the withdrawn value

---
+++ src/main/java/com/neves_eduardo/cash_machine/exception/NoNotesForTransactionException.java
@@ -0,0 +1,8 @@
+package com.neves_eduardo.cash_machine.exception;
+
+public class NoNotesForTransactionException extends RuntimeException {
+    public NoNotesForTransactionException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
