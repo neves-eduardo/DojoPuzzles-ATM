c34d9aef2862d356c7408188dedf7eb8faf52059
neves-eduardo <39205974+neves-eduardo@users.noreply.github.com>
2020-04-01 18:26:15 -0300

    Test/implementingtests (#4)
    * feat: CashMachine now subtracts from TotalCash
    
    For multiple consecutive withdrawals
    
    * feat: CashMachine now does not accept negative numbers
    
    * feat: adding exception to when there is no money at the machine
    
    * feat: adding validation to when the machine is empty
    
    * feat: making interaction loop
    
    * test: Implementing tests for the CashMachine class

--- src/main/java/com/neves_eduardo/cash_machine/CashMachine.java
+++ src/main/java/com/neves_eduardo/cash_machine/CashMachine.java
@@ -1,6 +1,7 @@
 package com.neves_eduardo.cash_machine;
 
 import com.google.common.collect.Iterables;
+import com.neves_eduardo.cash_machine.exception.MachineEmptyException;
 import com.neves_eduardo.cash_machine.exception.NoNotesForTransactionException;
 import org.apache.commons.lang3.EnumUtils;
 
@@ -20,23 +21,31 @@ public class CashMachine {
     }
 
     public Map<String,Integer> withdraw(int quantity) {
+        if(totalAmountOfCash == 0) {throw new MachineEmptyException("Machine Empty! Please use another one.");}
+
         Map<String, Integer> notesQuantity = new HashMap<>();
         int smallestAvailableNoteValue = listOfNotes.stream().filter(s -> s.getNumberOfNotesStored() != 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList()).get(0).getValue();
         int remaining = quantity;
 
+
+        if(quantity<0){throw new IllegalArgumentException("Invalid Input: Please insert only positive numbers");}
+
         if(quantity > totalAmountOfCash) {
-            throw new NoNotesForTransactionException("Not enough notes available for this withdrawal, maximum withdrawal amount: " + totalAmountOfCash);
+            throw new NoNotesForTransactionException(
+                    "Not enough notes available for this withdrawal, maximum withdrawal amount: " + totalAmountOfCash);
         }
         if(quantity % smallestAvailableNoteValue != 0) {
-            throw new NoNotesForTransactionException("No notes for this transaction, please insert a value that is multiple by " + smallestAvailableNoteValue);
+            throw new NoNotesForTransactionException(
+                    "No notes for this transaction, please insert a value that is multiple by " + smallestAvailableNoteValue);
         }
 
         for ( AvailableNotes note: listOfNotes) {
             int numberOfNotes = countNotes(remaining,note);
             remaining -= numberOfNotes*note.getValue();
-            notesQuantity.put(note.name(),numberOfNotes);
+            if(numberOfNotes != 0) notesQuantity.put(note.name(),numberOfNotes);
             if(remaining == 0) break;
         }
+        totalAmountOfCash -= quantity;
         return notesQuantity;
     }
 
@@ -44,7 +53,6 @@ public class CashMachine {
         int numberOfNotesWithdrawn = value/note.getValue();
         if(numberOfNotesWithdrawn > note.getNumberOfNotesStored()) {numberOfNotesWithdrawn = note.getNumberOfNotesStored();}
         note.setNumberOfNotesStored(note.getNumberOfNotesStored() - numberOfNotesWithdrawn);
-        System.out.println(note.getNumberOfNotesStored());
         return numberOfNotesWithdrawn;
     }
 
