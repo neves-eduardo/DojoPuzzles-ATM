1722e9e7021a8c7836fdfef3e1ed3f0b40d290b3
neves-eduardo <39205974+neves-eduardo@users.noreply.github.com>
2020-04-01 14:10:05 -0300

    Feat/makingnotesfinite (#3)
    * feat: now available notes class also contains a stored notes attribute
    
    * feat: adding validations for finite notes
    
    * chore: returning a message when there's no enough money in the ATM to withdraw

--- src/main/java/com/neves_eduardo/cash_machine/AvailableNotes.java
+++ src/main/java/com/neves_eduardo/cash_machine/AvailableNotes.java
@@ -1,16 +1,24 @@
 package com.neves_eduardo.cash_machine;
 
 public enum AvailableNotes {
-    HUNDRED(100), FIFTY(50),TWENTY(20), TEN(10);
+    HUNDRED(100,100), FIFTY(50,100), TWENTY(20,100), TEN(10,100);
     private int value;
+    private int numberOfNotesStored;
 
-    public int getValue()
-    {
+    public int getValue() {
         return this.value;
     }
 
-    private AvailableNotes(int value)
-    {
+    public int getNumberOfNotesStored() {
+        return this.numberOfNotesStored;
+    }
+
+    public void setNumberOfNotesStored(int numberOfNotesStored) {
+        this.numberOfNotesStored = numberOfNotesStored;
+    }
+
+    private AvailableNotes(int value, int numberOfNotesStored) {
         this.value = value;
+        this.numberOfNotesStored = numberOfNotesStored;
     }
 }
--- src/main/java/com/neves_eduardo/cash_machine/CashMachine.java
+++ src/main/java/com/neves_eduardo/cash_machine/CashMachine.java
@@ -8,20 +8,27 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CashMachine {
     private List<AvailableNotes> listOfNotes;
+    private int totalAmountOfCash;
     public CashMachine() {
         this.listOfNotes = EnumUtils.getEnumList(AvailableNotes.class);
         listOfNotes.sort(Comparator.comparingInt(AvailableNotes::getValue).reversed());
+        totalAmountOfCash = listOfNotes.stream().mapToInt(n -> n.getValue() * n.getNumberOfNotesStored()).sum();
     }
 
     public Map<String,Integer> withdraw(int quantity) {
         Map<String, Integer> notesQuantity = new HashMap<>();
+        int smallestAvailableNoteValue = listOfNotes.stream().filter(s -> s.getNumberOfNotesStored() != 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList()).get(0).getValue();
         int remaining = quantity;
 
-        if(quantity % Iterables.getLast(listOfNotes).getValue() != 0) {
-            throw new NoNotesForTransactionException("No notes for this transaction, please insert a value that is multiple by " + Iterables.getLast(listOfNotes).getValue());
+        if(quantity > totalAmountOfCash) {
+            throw new NoNotesForTransactionException("Not enough notes available for this withdrawal, maximum withdrawal amount: " + totalAmountOfCash);
+        }
+        if(quantity % smallestAvailableNoteValue != 0) {
+            throw new NoNotesForTransactionException("No notes for this transaction, please insert a value that is multiple by " + smallestAvailableNoteValue);
         }
 
         for ( AvailableNotes note: listOfNotes) {
@@ -34,7 +41,11 @@ public class CashMachine {
     }
 
     private int countNotes(int value, AvailableNotes note) {
-        return value/note.getValue();
+        int numberOfNotesWithdrawn = value/note.getValue();
+        if(numberOfNotesWithdrawn > note.getNumberOfNotesStored()) {numberOfNotesWithdrawn = note.getNumberOfNotesStored();}
+        note.setNumberOfNotesStored(note.getNumberOfNotesStored() - numberOfNotesWithdrawn);
+        System.out.println(note.getNumberOfNotesStored());
+        return numberOfNotesWithdrawn;
     }
 
 
